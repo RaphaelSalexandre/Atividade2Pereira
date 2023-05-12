019e1670e603f158fcc687ed7b2747457356ec18
Raphael S. Alexandre <102632061+RaphaelSalexandre@users.noreply.github.com>
2023-05-11 21:28:42 -0300

    Tarefa Complementar - Aula dia 27-03-2023
    #include <stdio.h>
    
    int main() {
       int valor, valor1, valor inteiro;
    
       printf("Digite o valor : ");
       scanf("%d", &valor);
       printf("Digite o valor 1: ");
       scanf("%d", &valor1);
       printf("Digite o valor inteiro: ");
       scanf("%d", &valorinteiro);
    
       if (valor >= valor1 && valor >= valorinteiro) {
          printf("Maior valor recebido: %d\n", valor);
       } else if (valor2 >= valor1 && valor2 >= valor inteiro) {
          printf("Maior valor recebido: %d\n", valor1);
       } else {
          printf("Maior valor recebido: %d\n", valor inteiro);
       }
    
       if (valor1 <= valor1 && valor <= valor inteiro) {
          printf("Menor valor recebido: %d\n", valor);
       } else if (valor2 <= valor1 && valor2 <= valorinteiro) {
          printf("Menor valor recebido: %d\n", valor1);
       } else {
          printf("Menor valor recebido: %d\n", valorinteiro);
       }
    
       return 0;
    }

---
+++ atividade2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+   int valor, valor1, valor inteiro;
+
+   printf("Digite o valor : ");
+   scanf("%d", &valor);
+   printf("Digite o valor 1: ");
+   scanf("%d", &valor1);
+   printf("Digite o valor inteiro: ");
+   scanf("%d", &valorinteiro);
+
+   if (valor >= valor1 && valor >= valorinteiro) {
+      printf("Maior valor recebido: %d\n", valor);
+   } else if (valor2 >= valor1 && valor2 >= valor inteiro) {
+      printf("Maior valor recebido: %d\n", valor1);
+   } else {
+      printf("Maior valor recebido: %d\n", valor inteiro);
+   }
+
+   if (valor1 <= valor1 && valor <= valor inteiro) {
+      printf("Menor valor recebido: %d\n", valor);
+   } else if (valor2 <= valor1 && valor2 <= valorinteiro) {
+      printf("Menor valor recebido: %d\n", valor1);
+   } else {
+      printf("Menor valor recebido: %d\n", valorinteiro);
+   }
+
+   return 0;
+}
+
